2d7734a5f29bd3e6bdea0a51a603fc5cf91d5102
Blake Scott and Nathan Otto <ottoetc@gmail.com>
2016-04-06 15:11:41 -0700

    add cart display page

--- app/router.js
+++ app/router.js
@@ -7,6 +7,7 @@ const Router = Ember.Router.extend({
 
 Router.map(function() {
   this.route('product-page', {path: '/product-page/:product_id'});
+  this.route('cart-page');
 });
 
 export default Router;
