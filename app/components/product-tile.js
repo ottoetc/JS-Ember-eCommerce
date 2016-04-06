48e29ee09bf5fce464a8ae4d2010e3397043b333
Blake Scott and Nathan Otto <ottoetc@gmail.com>
2016-04-06 13:57:57 -0700

    add add-to-cart button

--- app/components/product-tile.js
+++ app/components/product-tile.js
@@ -1,7 +1,13 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  shoppingCart: Ember.inject.service(),
 
+  actions: {
+    addToCart(item) {
+      this.get('shoppingCart').add(item);
+    }
+  }
 
 
 });
