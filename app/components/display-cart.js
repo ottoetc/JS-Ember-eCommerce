8ab28ecebfa53a520bc479f1694e33e2a7f0b6a6
Blake Scott <blakerscott@gmail.com>
2016-04-07 15:31:25 -0700

    add functionality for shopping cart to display cart total

--- app/components/display-cart.js
+++ app/components/display-cart.js
@@ -2,4 +2,13 @@ import Ember from 'ember';
 
 export default Ember.Component.extend({
   shoppingCart: Ember.inject.service(),
+  total: Ember.computed('shoppingCart.items.length', function() {
+  var totalPrice = 0;
+  var shoppingCart = this.get('shoppingCart');
+
+  shoppingCart.items.forEach(function(item) {
+    totalPrice += item.get('price');
+    });
+    return totalPrice;
+  }),
 });
--- app/services/shopping-cart.js
+++ app/services/shopping-cart.js
@@ -2,8 +2,9 @@ import Ember from 'ember';
 
 export default Ember.Service.extend({
   items: [],
+  // var totalCost = 0,
 
-  add(item) {
-    this.get('items').pushObject(item);
+    add(item) {
+      this.get('items').pushObject(item)
   }
 });
